6e4d4a835e2a9e0eea405595bb905bc19e447f33
Econi Noel Aita <noelaita03@gmail.com>
2025-08-25 11:53:06 +0300

    Added a recipe list to the app

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,6 +1,7 @@
 // src/App.jsx
 import React from "react";
 import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeList from "./components/RecipeList"
 import { useRecipeStore } from "./recipeStore";
 
 const App = () => {
