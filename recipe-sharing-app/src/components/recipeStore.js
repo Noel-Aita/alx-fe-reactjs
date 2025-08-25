f8c27b0430be88568dfe0b7d2ca1d400364bec91
Econi Noel Aita <noelaita03@gmail.com>
2025-08-25 11:57:54 +0300

    Added the file RecipeStore.js

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -4,4 +4,5 @@ import { create } from "zustand";
 export const useRecipeStore = create((set) => ({
   recipes: [],
   addRecipe: (recipe) => set((state) => ({ recipes: [...state.recipes, recipe] })),
+  setRecipes: (newRecipes) => set({ recipes: newRecipes }),
 }));
