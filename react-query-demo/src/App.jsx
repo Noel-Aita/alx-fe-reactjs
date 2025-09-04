41ada1627f8c3c8b05cf6aa154ac2f6b3241d642
Econi Noel Aita <noelaita03@gmail.com>
2025-09-05 00:39:35 +0400

    installed data handling with React Query

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from 'react'
-import './App.css'
 import PostsComponent from "./components/PostsComponent";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create the QueryClient instance
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-center text-2xl font-bold my-4">React Query Demo</h1>
-      <PostsComponent />
-    </div>
+    // Provide the QueryClient to your app
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1 className="text-center text-2xl font-bold my-4">
+          React Query Demo
+        </h1>
+        <PostsComponent />
+      </div>
+    </QueryClientProvider>
   );
 }
 
