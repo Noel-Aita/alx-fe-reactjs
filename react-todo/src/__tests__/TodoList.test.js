011be2ff0f9799e2eed54cad45164e1ce1d9b58f
Econi Noel Aita <noelaita03@gmail.com>
2025-09-05 07:22:13 +0300

    added the TodoList.test.jsx

--- react-todo/src/__tests__/TodoList.test.js
+++ react-todo/src/__tests__/TodoList.test.js
@@ -5,7 +5,8 @@ import TodoList from "../components/TodoList";
 describe("TodoList Component", () => {
   test("renders initial todos", () => {
     render(<TodoList />);
-    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    screen.getByText("Learn React");
+    screen.getByText("Build a Todo App");
   });
 
   test("adds a new todo", async () => {
@@ -13,24 +14,26 @@ describe("TodoList Component", () => {
     const input = screen.getByPlaceholderText("Add a new todo");
     const button = screen.getByText("Add");
 
-    await userEvent.type(input, "New Todo");
+    await userEvent.type(input, "Test Todo");
     fireEvent.click(button);
 
-    expect(screen.getByText("New Todo")).toBeInTheDocument();
+    screen.getByText("Test Todo");
   });
 
   test("toggles a todo completed state", () => {
     render(<TodoList />);
     const todo = screen.getByText("Learn React");
+
     fireEvent.click(todo);
-    expect(todo).toHaveClass("line-through");
+    expect(todo.style.textDecoration).toBe("line-through");
   });
 
   test("deletes a todo", () => {
     render(<TodoList />);
     const todo = screen.getByText("Learn React");
     const deleteButton = todo.querySelector("button");
+
     fireEvent.click(deleteButton);
-    expect(screen.queryByText("Learn React")).not.toBeInTheDocument();
+    expect(screen.queryByText("Learn React")).toBeNull();
   });
 });
